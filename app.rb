c0ab1fd05a21648c2cecc53f3db6f67756bf6dbe
sahana-mangipudi <smangipudi@uchicago.edu>
2025-02-04 18:28:56 +0000

    64% accuracy

--- app.rb
+++ app.rb
@@ -1,9 +1,65 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+get("/square/new") do
+  erb :square
+end
+
+post("/square") do
+  number = params[:number].to_f
+  @result = number ** 2
+  erb :result
+end
+
+
+get("/square_root/new") do
+  erb :square_root
+end
+
+post("/square_root") do
+  number = params[:number].to_f
+  @result = Math.sqrt(number)
+  erb :result
+end
+
+
+get ("/random/new") do
+  erb :random
+end
+
+#generates a random number between specified minimum and maximum 
+post ("/random") do
+  min = params[:min].to_f
+  max = params[:max].to_f
+  @result = rand(min..max)
+  erb :result
+end
+
+
+get ("/payment/new") do
+  erb :payment
+end
+
+post "/payment/result" do
+  apr = params[:apr].to_f / 100
+  years = params[:years].to_i
+  principal = params[:principal].to_f
+
+  monthly_rate = apr / 12
+  num_payments = years * 12
+  monthly_payment = (principal * monthly_rate) / (1 - (1 + monthly_rate)**(-num_payments))
+
+  formatted_payment = monthly_payment.to_fs(:currency)
+  formatted_principal = principal_payment.to_fs(:currency) 
+  formatted_apr = (apr * 100).round(4).to_s + "%"
+
+  @results = {
+    monthly_payment: formatted_payment,
+    principal: formatted_principal,
+    apr: formatted_apr,
+    years: years
+  }
+
+  erb :result
 end
